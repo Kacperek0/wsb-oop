c5ee91578cb90080cabf21ff6ee2ae1111daed9f
Kacper Szczepanek <kacper.szczepanek@nordcloud.com>
2020-10-10 23:47:51 +0200

    hotfix

--- OOP_homework/Address.cs
+++ OOP_homework/Address.cs
@@ -3,8 +3,24 @@ namespace OOP_homework
 {
     public class Address
     {
+        string street, postalCode, city, country;
+        int homeNumber, flatNumber;
+
         public Address()
         {
+
+        }
+
+        public void AddAdress(string address)
+        {
+
+        }
+
+        public void ReadAddress()
+        {
+            Console.Write("{0}/{1} {2} St.", this.homeNumber, this.flatNumber, this.street);
+            Console.Write("{0} {1}", this.postalCode, this.city);
+            Console.Write(this.country);
         }
     }
 }
--- OOP_homework/Person.cs
+++ OOP_homework/Person.cs
@@ -3,8 +3,14 @@ namespace OOP_homework
 {
     public class Person
     {
-        string Name;
-        string Surname;
+        string name, surname, address;
+        Address a1 = new Address();
+
+        public string Address
+        {
+            get;
+            set;
+        }
 
         public Person()
         {
@@ -13,14 +19,18 @@ namespace OOP_homework
 
         public void Introduce()
         {
-            Console.WriteLine("Hi! My name is {0} {1}", this.Name, this.Surname);
+            Console.WriteLine("Hi! My name is {0} {1}", this.name, this.surname);
         }
 
         public void SetData(string name, string surname)
         {
-            this.Name = name;
-            this.Surname = surname;
+            this.name = name;
+            this.surname = surname;
 
         }
+
+
+
+
     }
 }
