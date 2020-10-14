cd29e5125d2cd69970fbcf71a09d3ad49e0f0755
Kacper Szczepanek <kacper.szczepanek@nordcloud.com>
2020-10-14 23:14:19 +0200

    task 13 remodel

--- OOP_homework/Address.cs
+++ OOP_homework/Address.cs
@@ -3,8 +3,19 @@ namespace OOP_homework
 {
     public class Address
     {
-        string street, postalCode, city, country;
-        int homeNumber, flatNumber;
+        public string Street { get; set; }
+        public int HouseNo { get; set; }
+        public int? FlatNo { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string PostalCode { get; set; }
+        public string PostalAddress {
+            get
+            {
+                return $"{HouseNo}{(FlatNo.HasValue ? "/" + FlatNo : "")}{Environment.NewLine}{PostalCode} {City}{Environment.NewLine}{Country}";
+            }
+        }
+
 
         public Address()
         {
